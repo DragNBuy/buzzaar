96334b6c97b0696fa86cf51a0be2ce458d594502
codelotusr <smaliukasdeividas@gmail.com>
2024-10-08 14:44:11 +0300

    Reminder change

--- buzzaar-backend/buzzaar_core/buzzaar_core/settings.py
+++ buzzaar-backend/buzzaar_core/buzzaar_core/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-#9&xu0&1moq7sstr4-h2x#jg!i)wj0x68vs3w*-74e!+m3sd9%"
+SECRET_KEY = "django-insecure-#9&xu0&1moq7sstr4-h2x#jg!i)wj0x68vs3w*-74e!+m3sd9%"  # REMEMBER TO CHANGE CAUSE ACCIDENTALLY COMMITED
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
