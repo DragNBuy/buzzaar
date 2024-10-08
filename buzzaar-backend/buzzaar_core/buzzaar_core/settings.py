e4183c09e5b903f9ceec65ab470636a565cf681a
codelotusr <smaliukasdeividas@gmail.com>
2024-10-08 17:15:38 +0300

    Necessary apps created

--- buzzaar-backend/buzzaar_core/buzzaar_core/settings.py
+++ buzzaar-backend/buzzaar_core/buzzaar_core/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # need to add all our apps + installed module apps
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Europe/Vilnius"
 
 USE_I18N = True
 
