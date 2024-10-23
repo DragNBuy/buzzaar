124cad71e12561d709e3cd53909665849035e805
codelotusr <smaliukasdeividas@gmail.com>
2024-10-23 23:20:20 +0300

    Custom user model

--- buzzaar-backend/buzzaar_core/users/models.py
+++ buzzaar-backend/buzzaar_core/users/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
