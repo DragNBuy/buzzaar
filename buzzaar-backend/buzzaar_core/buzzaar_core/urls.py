51b089da4478e78df8a94d966384d6f72ac3e80d
codelotusr <smaliukasdeividas@gmail.com>
2024-10-24 12:39:02 +0300

    Updated urls to fix Angular standards

--- buzzaar-backend/buzzaar_core/buzzaar_core/urls.py
+++ buzzaar-backend/buzzaar_core/buzzaar_core/urls.py
@@ -20,6 +20,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("auth/", include("dj_rest_auth.urls")),
-    path("auth/registration/", include("dj_rest_auth.registration.urls")),
+    path("api/auth/", include("dj_rest_auth.urls")),
+    path("api/auth/registration/", include("dj_rest_auth.registration.urls")),
 ]
