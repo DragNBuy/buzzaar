6beecfcfa62cd65708ac02cb618e16cb26948ed1
codelotusr <smaliukasdeividas@gmail.com>
2024-10-31 18:43:50 +0000

    Create login test

--- buzzaar-backend/buzzaar_core/users/tests/test_authentication.py
+++ buzzaar-backend/buzzaar_core/users/tests/test_authentication.py
@@ -23,3 +23,19 @@ def test_user_registration(api_client):
     assert response.status_code == status.HTTP_201_CREATED
     assert response.data["user"]["email"] == "newuser@gmail.com"
     assert response.data["user"]["username"] == "newuser"
+
+
+@pytest.mark.authentication
+@pytest.mark.django_db
+def test_user_login(api_client, user):
+    """
+    Test that a user can log in successfully.
+    """
+
+    url = reverse("rest_login")
+    data = {"email": user.email, "password": "password123"}
+    response = api_client.post(url, data, format="json")
+
+    assert response.status_code == status.HTTP_200_OK
+    assert "buzzaar_access_token" in response.cookies
+    assert "buzzaar_refresh_token" in response.cookies
