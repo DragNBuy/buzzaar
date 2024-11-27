2357734427cf5d31afdfc90d300b1a82dabb3682
Erikas <xerikasxx@gmail.com>
2024-11-27 23:13:20 +0200

    Configured ASGI to include websocket routing

--- buzzaar-backend/buzzaar_core/buzzaar_core/asgi.py
+++ buzzaar-backend/buzzaar_core/buzzaar_core/asgi.py
@@ -9,8 +9,19 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 
 import os
 
+from channels.routing import URLRouter, ProtocolTypeRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
+from messaging.routing import websocket_urlpatterns 
+from .middleware import TokenAuthMiddleware
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "buzzaar_core.settings")
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AllowedHostsOriginValidator(
+        TokenAuthMiddleware(
+            URLRouter(websocket_urlpatterns)
+        )
+    )
+})
