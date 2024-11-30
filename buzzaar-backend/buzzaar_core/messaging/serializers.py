4d51e80a664d33e5fef1e9e68118ecf534fbb236
Erikas <xerikasxx@gmail.com>
2024-11-30 17:19:36 +0200

    Messages serializer now returns just the users' ID, not the entire users' body

--- buzzaar-backend/buzzaar_core/messaging/serializers.py
+++ buzzaar-backend/buzzaar_core/messaging/serializers.py
@@ -9,23 +9,23 @@ class MessageSerializer(serializers.ModelSerializer):
         exclude = ('conversation_id',)
 
 class ConversationListSerializer(serializers.ModelSerializer):
-    initiator = CustomUserSerializer()
-    receiver = CustomUserSerializer()
+    initiator_id = serializers.IntegerField(read_only=True)
+    receiver_id = serializers.IntegerField(read_only=True)
     last_message = serializers.SerializerMethodField()
 
     class Meta:
         model = Conversation
-        fields = ['initiator', 'receiver', 'last_message']
+        fields = ['initiator_id', 'receiver_id', 'last_message']
 
     def get_last_message(self, instance):
         message = instance.message_set.first()
         return MessageSerializer(instance=message)
 
 class ConversationSerializer(serializers.ModelSerializer):
-    initiator = CustomUserSerializer()
-    receiver = CustomUserSerializer()
+    initiator_id = serializers.IntegerField(read_only=True)
+    receiver_id = serializers.IntegerField(read_only=True)
     message_set = MessageSerializer(many=True)
 
     class Meta:
         model = Conversation
-        fields = ['initiator', 'receiver', 'message_set']
+        fields = ['initiator_id', 'receiver_id', 'message_set']
