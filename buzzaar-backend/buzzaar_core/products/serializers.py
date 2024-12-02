182587177dc72f6b20932f9c0f14ea71eebea005
codelotusr <smaliukasdeividas@gmail.com>
2024-12-02 16:20:17 +0000

    fetch owner from request

--- buzzaar-backend/buzzaar_core/products/serializers.py
+++ buzzaar-backend/buzzaar_core/products/serializers.py
@@ -1,10 +1,11 @@
+from datetime import datetime
+
 from rest_framework import serializers
 
 from product_categories.models import ProductCategory
 from users.models import CustomUser
 
 from .models import Product
-from datetime import datetime
 
 
 class ProductSerializer(serializers.ModelSerializer):
@@ -26,22 +27,17 @@ class ProductSerializer(serializers.ModelSerializer):
             "initial_asking_price",
             "date_created",
             "visible",
-            "owner_id",
             "owner",
         ]
         read_only_fields = ["id", "date_created", "visible", "owner", "category"]
-
-    def validate_owner_id(self, value):
-        try:
-            return CustomUser.objects.get(pk=value)
-        except CustomUser.DoesNotExist:
-            raise serializers.ValidationError(f"User with id={value} does not exist.")
 
     def validate_category_id(self, value):
         try:
             return ProductCategory.objects.get(pk=value)
         except ProductCategory.DoesNotExist:
-            raise serializers.ValidationError(f"Product with id={value} does not exist.")
+            raise serializers.ValidationError(
+                f"Product with id={value} does not exist."
+            )
 
     def validate_initial_asking_price(self, value):
         if value >= 10**6 or value < 0:
@@ -51,7 +47,7 @@ class ProductSerializer(serializers.ModelSerializer):
         return value
 
     def create(self, validated_data):
-        owner = validated_data.pop("owner_id")
+        owner = self.context["request"].user
         category = validated_data.pop("category_id")
         date = datetime.now()
         return Product.objects.create(
@@ -59,5 +55,5 @@ class ProductSerializer(serializers.ModelSerializer):
             category=category,
             date_created=date,
             visible=True,
-            **validated_data
+            **validated_data,
         )
