866ea810c6a8e18428cf701362af557c8cf0aa6b
Erikas <xerikasxx@gmail.com>
2024-12-08 22:12:15 +0200

    Changed token middleware into something that actually works

--- buzzaar-backend/buzzaar_core/buzzaar_core/middleware.py
+++ buzzaar-backend/buzzaar_core/buzzaar_core/middleware.py
@@ -1,15 +1,22 @@
-from django.contrib.auth.models import AnonymousUser
+import jwt
 from channels.db import database_sync_to_async
-from rest_framework.authtoken.models import Token
 from channels.middleware import BaseMiddleware
+from django.contrib.auth.models import AnonymousUser
+from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
+from rest_framework_simplejwt.tokens import UntypedToken
+
+from users.models import CustomUser as User
+
 
 @database_sync_to_async
 def get_user(token_key):
     try:
-        token = Token.objects.get(key=token_key)
-        return token.user
-    except Token.DoesNotExist:
+        payload = UntypedToken(token_key)
+        user_id = payload.get("user_id")
+        return User.objects.get(id=user_id)
+    except (User.DoesNotExist, InvalidToken, TokenError, jwt.ExpiredSignatureError):
         return AnonymousUser()
+
 
 class TokenAuthMiddleware(BaseMiddleware):
 
@@ -17,9 +24,13 @@ class TokenAuthMiddleware(BaseMiddleware):
         self.inner = inner
 
     async def __call__(self, scope, receive, send):
-        headers = dict(scope['headers'])
-        if b'authorization' in headers:
-            token_name, token_key = headers[b'authorization'].decode().split()
-            if token_name == 'Token':
-                scope['user'] = await get_user(token_key)
+        headers = dict(scope["headers"])
+        if b"cookie" in headers:
+            cookie_items = dict(
+                x.split("=")
+                for x in headers[b"cookie"].decode().replace(" ", "").split(";")
+            )
+            if "buzzaar_access_token" in cookie_items:
+                token = cookie_items["buzzaar_access_token"]
+                scope["user"] = await get_user(token)
             return await super().__call__(scope, receive, send)
