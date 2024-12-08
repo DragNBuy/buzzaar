ce7e8c00a3ffbe871e8a7aac0468a280e58a785f
Erikas <xerikasxx@gmail.com>
2024-12-08 22:12:37 +0200

    Cleaned up imports

--- buzzaar-backend/buzzaar_core/messaging/consumers.py
+++ buzzaar-backend/buzzaar_core/messaging/consumers.py
@@ -1,15 +1,14 @@
 import base64
 import json
 import secrets
-from datetime import datetime
 
-from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.db import database_sync_to_async
+from channels.generic.websocket import AsyncWebsocketConsumer
 from django.core.files.base import ContentFile
 
-from users.models import CustomUser as User
-from .models import Message, Conversation
+from .models import Conversation, Message
 from .serializers import MessageSerializer
+
 
 class ChatConsumer(AsyncWebsocketConsumer):
 
@@ -17,57 +16,49 @@ class ChatConsumer(AsyncWebsocketConsumer):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = f"chat_{self.room_name}"
 
-        await self.channel_layer.group_add(
-            self.room_group_name, self.channel_name
-        )
+        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
         await self.accept()
-    
+
     async def disconnect(self, code):
-        await self.channel_layer.group_discard(
-            self.room_group_name, self.channel_name    
-        )
+        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
     async def receive(self, text_data=None, bytes_data=None):
-        
+
         text_data_json = json.loads(text_data)
 
         chat_type = {"type": "chat_message"}
         return_dict = {**chat_type, **text_data_json}
-        await self.channel_layer.group_send(
-            self.room_group_name, return_dict
-        )
+        await self.channel_layer.group_send(self.room_group_name, return_dict)
 
         async def chat_message(self, event):
             text_data_json = event.copy()
             text_data_json.pop("type")
             message = text_data_json["message"]
             attachment = text_data_json.get("attachment")
-            conversation = await database_sync_to_async(Conversation.objects.get)(id=int(self.room_name))
-            
-            sender = self.scope['user']
+            conversation = await database_sync_to_async(Conversation.objects.get)(
+                id=int(self.room_name)
+            )
+
+            sender = self.scope["user"]
 
             if attachment:
                 file_str, file_ext = attachment["data"], attachment["format"]
-                
+
                 file_data = ContentFile(
-                    base64.b64decode(file_str, name=f"{secrets.token_hex(8)}.{file_ext}")
+                    base64.b64decode(
+                        file_str, name=f"{secrets.token_hex(8)}.{file_ext}"
+                    )
                 )
-                
+
                 _message = await database_sync_to_async(Message.objects.create)(
                     sender=sender,
                     attachment=file_data,
                     text=message,
-                    conversation_id=conversation
+                    conversation_id=conversation,
                 )
             else:
                 _message = await database_sync_to_async(Message.objects.create)(
-                    sender=sender,
-                    text=message,
-                    conversation_id=conversation
+                    sender=sender, text=message, conversation_id=conversation
                 )
             serializer = MessageSerializer(instance=_message)
             await self.send(text_data=json.dumps(serializer.data))
-
-
-        
-        
