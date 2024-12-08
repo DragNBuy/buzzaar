307cd8e5222dd7b7a32a14e58fac452b2e405859
Almond <almondCloud@outlook.com>
2024-12-08 19:07:10 +0200

    Add UserLike filtering (available through query params)

--- buzzaar-backend/buzzaar_core/buzzaar_core/settings.py
+++ buzzaar-backend/buzzaar_core/buzzaar_core/settings.py
@@ -127,7 +127,10 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
     ],
-    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'
+    ],
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema"
 }
 
 REST_AUTH = {
