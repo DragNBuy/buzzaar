bbc9c9dbb3ba1c6343471e778827266811040ddb
Erikas <xerikasxx@gmail.com>
2024-12-10 18:53:25 +0200

    Changed default message sorting

--- buzzaar-backend/buzzaar_core/messaging/models.py
+++ buzzaar-backend/buzzaar_core/messaging/models.py
@@ -1,20 +1,21 @@
-from django.db import models
 from django.conf import settings
+from django.db import models
+
 
 class Conversation(models.Model):
 
     initiator = models.ForeignKey(
-        settings.AUTH_USER_MODEL, 
-        on_delete=models.SET_NULL, 
-        null=True, 
-        related_name="buyer"
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="buyer",
     )
 
     receiver = models.ForeignKey(
-        settings.AUTH_USER_MODEL, 
-        on_delete=models.SET_NULL, 
-        null=True, 
-        related_name="seller"
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="seller",
     )
 
     start_time = models.DateTimeField(auto_now_add=True)
@@ -25,14 +26,17 @@ class Message(models.Model):
     sender = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.SET_NULL,
-        null=True, 
-        related_name='sender'
+        null=True,
+        related_name="sender",
     )
 
     text = models.CharField(max_length=200, blank=True)
     attachment = models.FileField(blank=True)
-    conversation_id = models.ForeignKey(Conversation, on_delete=models.CASCADE,)
+    conversation_id = models.ForeignKey(
+        Conversation,
+        on_delete=models.CASCADE,
+    )
     timestamp = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ('-timestamp',) 
+        ordering = ("timestamp",)
